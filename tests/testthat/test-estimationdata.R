7e5b07574ebaedc2ae1451c435617ad9bc9b9734
TimBock <tim.bock@numbers.net.au>
2016-08-24 10:25:53 +1000

    Using CopyAttributes

--- tests/testthat/test-estimationdata.R
+++ tests/testthat/test-estimationdata.R
@@ -11,3 +11,17 @@ test_that("Single vs multiple imputation",
     expect_is(est$estimation.data, "list")
     expect_equal(length(est$estimation.data), 10)
 })
+
+test_that("Checking that label is retained",
+{
+    data("bank", package = "flipExampleData")
+    attr(bank$Overall, "label") <- "Overall satisfaction"
+
+    est <- EstimationData(Overall ~ Fees + Branch, bank, missing = "Imputation (replace missing values with estimates)")
+    expect_is(est$estimation.data, "data.frame")
+    est <- EstimationData(Overall ~ Fees + Branch, bank, missing = "Multiple imputation", m = 10)
+    expect_is(est$estimation.data, "list")
+    expect_equal(length(est$estimation.data), 10)
+})
+
+
