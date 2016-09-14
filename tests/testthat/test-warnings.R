19caca1dc428c15baf6b55ee4ffbb934b34d7b14
TimBock <tim.bock@numbers.net.au>
2016-09-14 17:18:51 +1000

    Adding CheckForUniqueVariableNames to EstimationData.  CE414

--- tests/testthat/test-warnings.R
+++
@@ -1,8 +0,0 @@
-context("warnings")
-
-
-test_that("CheckForUniqueVariableNames",
-{
-     expect_error(CheckForUniqueVariableNames(y ~ x + y + y))
-     expect_error(CheckForUniqueVariableNames(y ~ x), NA)
-})
