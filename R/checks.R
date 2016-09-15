f363c0575816f86ec3b6c78df6dd33d2ecee44c9
TimBock <tim.bock@numbers.net.au>
2016-09-15 16:39:34 +1000

    Improving testing of duplicate variables
    Dealing with x$y case

--- R/checks.R
+++ R/checks.R
@@ -2,11 +2,17 @@
 #'
 #' @description Checks that the same variable name is not used twice in a formula.
 #' @param formula A \code{\link{formula}}.
+#' @importFrom flipFormat TrimWhitespace
 #' @export
 #'
 CheckForUniqueVariableNames <- function(formula)
 {
-    n.times <- table(all.vars(formula, unique = FALSE))
+    formula.as.character <- as.character(formula)
+    formula.as.character <- gsub('\\$', '', formula.as.character)
+    dep <- formula.as.character[2]
+    ind <- TrimWhitespace(strsplit(formula.as.character[3], "\\+")[[1]])
+    names <- c(dep, ind)
+    n.times <- table(names)
     max.times <- max(n.times)
     if (max.times > 1)
     {
--- tests/testthat/test-checks.R
+++ tests/testthat/test-checks.R
@@ -4,5 +4,8 @@ context("warnings")
 test_that("CheckForUniqueVariableNames",
 {
      expect_error(CheckForUniqueVariableNames(y ~ x + y + y))
+     expect_error(CheckForUniqueVariableNames(z$y ~ z$x + z$y + z$y))
      expect_error(CheckForUniqueVariableNames(y ~ x), NA)
+     expect_error(CheckForUniqueVariableNames(z$y ~ z$x), NA)
 })
+
