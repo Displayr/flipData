272a6df46868400365a11a1df306de7d422ab25c
jakehoare <jake_hoare@hotmail.com>
2018-08-24 16:58:34 +1000

    Add Observed method for MachineLearningEnsemble

--- R/observed.R
+++ R/observed.R
@@ -1,12 +1,14 @@
 
-#' \code{observed} The variable used as the outcome when fitting a model. .
+#' \code{Observed}
+#'
+#' Returns the variable used as the outcome when fitting a model.
 #' @param x A model.
 #' @details Extracted for the entire data set (i.e., not just the specific values used in the fitting).
 #' @export
 Observed <- function(x) UseMethod("Observed", x)
 
 #' @inheritParams Observed
-#' @describeIn Observed Exctracts the dependant variable from an object from its formula.
+#' @describeIn Observed Extracts the dependant variable from an object from its formula.
 #' @importFrom stats formula
 #' @export
 Observed.default <- function(x)
@@ -16,3 +18,10 @@ Observed.default <- function(x)
     x$model[, all.vars(formula(x))[1]]
 }
 
+#' @inheritParams Observed
+#' @describeIn Observed Extracts the dependant variable directly from an object.
+#' @export
+Observed.MachineLearningEnsemble <- function(x)
+{
+    x$outcome
+}
