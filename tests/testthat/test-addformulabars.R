9582aaa01e74ce46e5bcde4854ccbda1f9ced803
Mathew W. McLean <mathew.w.mclean@gmail.com>
2018-10-23 10:37:30 +1100

    Fix typos in unit tests; DS-1898

--- tests/testthat/test-addformulabars.R
+++ tests/testthat/test-addformulabars.R
@@ -1,4 +1,6 @@
-test("AddFormulaBars factors and continuous",
+context("AddFormulaBars")
+
+test_that("AddFormulaBars factors and continuous",
 {
     dat <- data.frame(c1 = 1:3, c2 = rnorm(3), f1 = factor(1:3), f2 = ordered(1:3))
     frml <- ~c1+c2+f1+f2
@@ -6,7 +8,7 @@ test("AddFormulaBars factors and continuous",
     expect_equal(AddFormulaBars(frml, dat), as.formula("~c1+c2+(1|f1)+(1|f2)"))
 })
 
-test("AddFormulaBars continuous only",
+test_that("AddFormulaBars continuous only",
 {
     dat <- data.frame(c1 = 1:3, c2 = rnorm(3), f1 = factor(1:3), f2 = ordered(1:3))
     frml <- ~c1
@@ -16,7 +18,7 @@ test("AddFormulaBars continuous only",
     expect_equal(AddFormulaBars(frml, dat), frml)
 })
 
-test("AddFormulaBars factors and continuous",
+test_that("AddFormulaBars factors and continuous",
 {
     dat <- data.frame(c1 = 1:3, c2 = rnorm(3), f1 = factor(1:3), f2 = ordered(1:3))
     frml <- ~f1
