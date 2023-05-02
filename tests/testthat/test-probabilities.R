324a4398b0803ceed6de71a2e2f735ce656ba783
jrwishart <opensource@displayr.com>
2023-05-02 17:53:29 +1000

    DS-4360 Handle MachineLearningEnsemble properly in Probabilities

--- tests/testthat/test-probabilities.R
+++ tests/testthat/test-probabilities.R
@@ -11,24 +11,35 @@ test_that("Probabilities", {
     # Data.frame empty
     invalid.input <- lapply(valid.classes, function(x) structure(list(model = data.frame()), class = x))
     expected.err <- paste0(sQuote("newdata"), " must be a data.frame with at least one observation")
-    for (input in invalid.input) {
+    names(invalid.input) <- valid.classes
+    model.only <- match(c("CART", "Regression", "MachineLearning"), valid.classes, nomatch = 0L)
+    regular.inputs <- invalid.input[model.only] # Remove the MachineLearningEnsemble
+    for (input in regular.inputs) {
         expect_error(validateProbabilityArguments(input, NULL),
                      expected.err)
         expect_error(Probabilities(input, NULL),
                      expected.err)
     }
+    # model element ignored in MachineLearningEnsemble
+    expect_silent(validateProbabilityArguments(invalid.input[["MachineLearningEnsemble"]]))
     invalid.input <- lapply(invalid.input, function(x) {
         x[["model"]] <- matrix(1, ncol = 1)
         x
     })
-    for (input in invalid.input) {
+    names(invalid.input) <- valid.classes
+    model.only <- match(c("CART", "Regression", "MachineLearning"), valid.classes, nomatch = 0L)
+    regular.inputs <- invalid.input[model.only] # Remove the MachineLearningEnsemble
+    for (input in regular.inputs) {
         expect_error(validateProbabilityArguments(input, NULL),
                      expected.err)
         expect_error(Probabilities(input, NULL),
                      expected.err)
     }
+    # model element ignored in MachineLearningEnsemble
+    expect_silent(validateProbabilityArguments(invalid.input[["MachineLearningEnsemble"]]))
     # Valid inputs
     valid.input <- lapply(valid.classes, function(x) structure(list(model = data.frame(x = 1)), class = x))
+    names(valid.input) <- valid.classes
     valid.input.but.no.method.err <- paste0("object not supported")
     for (input in valid.input) {
         expect_silent(validateProbabilityArguments(input, NULL))
