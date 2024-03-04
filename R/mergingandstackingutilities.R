aef153078f4c81a325dd19ccf51cdcea176ab4bd
mwm-displayr <147356510+mwm-displayr@users.noreply.github.com>
2024-03-04 14:36:09 +1100

    DS-5464: Fixup copy/paste error

--- R/mergingandstackingutilities.R
+++ R/mergingandstackingutilities.R
@@ -357,7 +357,7 @@ isIntegerValued <- function(x, val.attr = NULL)
     if (is.null(val.attr))
         val.attr <- attr(x, "labels", exact = TRUE)
     value.attrs.not.nan <- is.null(val.attr) || !any(is.nan(val.attr))
-    vals.not.nan <- !all(abs(result) <= .Machine$integer.max, na.rm = TRUE)
+    vals.not.nan <- !all(abs(x) <= .Machine$integer.max, na.rm = TRUE)
     if (value.attrs.not.nan && vals.not.nan && is.numeric(x))
     {
         x.without.na <- removeNA(x)
