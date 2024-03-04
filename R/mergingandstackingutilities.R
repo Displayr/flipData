81ce6c9e5191a342ffa272ea0f52d734ac68389c
mwm-displayr <147356510+mwm-displayr@users.noreply.github.com>
2024-03-04 14:33:29 +1100

    DS-5236: Fixup refactor of isIntegerValued

--- R/mergingandstackingutilities.R
+++ R/mergingandstackingutilities.R
@@ -355,7 +355,7 @@ splitByComma <- function(input.text, ignore.commas.in.parentheses = FALSE)
 isIntegerValued <- function(x, val.attr = NULL)
 {
     if (is.null(val.attr))
-        val.attr <- attr(input.var, "labels", exact = TRUE)
+        val.attr <- attr(x, "labels", exact = TRUE)
     value.attrs.not.nan <- is.null(val.attr) || !any(is.nan(val.attr))
     vals.not.nan <- !all(abs(result) <= .Machine$integer.max, na.rm = TRUE)
     if (value.attrs.not.nan && vals.not.nan && is.numeric(x))
