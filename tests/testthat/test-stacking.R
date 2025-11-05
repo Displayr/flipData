b330d6b368f976e9c0097389f1ebc14e8f361f8a
Justin Yap <justin.yap@displayr.com>
2025-11-05 12:56:07 +1100

    Fix

--- tests/testthat/test-stacking.R
+++ tests/testthat/test-stacking.R
@@ -329,14 +329,14 @@ test_that("stackWithCommonLabels", {
     val.attr <- 1:3
     names(val.attr) <- letters[1:3]
     v.val.attr <- rep(list(val.attr), length(v.names))
-    stacking.groups <- expect_warning(stackWithCommonLabels(common.labels.list,
+    expect_warning(stacking.groups <- stackWithCommonLabels(common.labels.list,
                                                             list(variable.names = v.names,
                                                                  variable.labels = v.labels,
                                                                  variable.types = v.types,
                                                                  variable.value.attributes = v.val.attr)),
-                   "Some variables could not be stacked due to mismatching ",
+                   paste0("Some variables could not be stacked due to mismatching ",
                    "variable types or value attributes. See Notes section in ",
-                   "output for more details.")
+                   "output for more details."))
     expect_equal(stacking.groups,
                  structure(integer(0), .Dim = c(0L, 3L),
                            unstackable.names = list(c("Q2_A", "Q2_B", "Q2_C"))))
@@ -345,14 +345,14 @@ test_that("stackWithCommonLabels", {
     val.attr.2 <- 4:6
     names(val.attr.2) <- letters[4:6]
     v.val.attr <- list(val.attr, val.attr, val.attr.2)
-    stacking.groups <- expect_warning(stackWithCommonLabels(common.labels.list,
+    expect_warning(stacking.groups <- stackWithCommonLabels(common.labels.list,
                                                             list(variable.names = v.names,
                                                                  variable.labels = v.labels,
                                                                  variable.types = v.types,
                                                                  variable.value.attributes = v.val.attr)),
-                   "Some variables could not be stacked due to mismatching ",
+                   paste0("Some variables could not be stacked due to mismatching ",
                    "variable types or value attributes. See Notes section in ",
-                   "output for more details.")
+                   "output for more details."))
     expect_equal(stacking.groups,
                  structure(integer(0), .Dim = c(0L, 3L),
                            unstackable.names = list(c("Q2_A", "Q2_B", "Q2_C"))))
